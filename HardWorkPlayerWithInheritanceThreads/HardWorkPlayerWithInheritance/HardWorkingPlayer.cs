87aca8d70aba077fb10bfd5cfccfa64b9d99750c
Станислав Горожанкин <sgorozhankin2@gmail.com>
2018-02-14 11:15:22 +0200

    HardWorkPlayer with Threads added

---
+++ HardWorkPlayerWithInheritanceThreads/HardWorkPlayerWithInheritance/HardWorkingPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HardWorkPlayerWithInheritance
+{
+    class HardWorkingPlayer : Players
+    {
+        private int LastMove;
+
+
+        public HardWorkingPlayer()
+        {
+            Name = "HardWorkingP";
+        }
+
+        public override void DoMove()
+        {
+            int counterPositionOnScreen = 0;
+
+            while (true)
+            {
+             //   HardPlayerHandler.WaitOne();
+
+                if (LastMove == 0)
+                {
+                    LastMove = MinValue;
+                    AddEnterdNumberInArray(LastMove);
+                }
+                else
+                {
+                    LastMove++;
+                    AddEnterdNumberInArray(LastMove);
+                }
+
+                if (LastMove == RezultValue)
+                {
+                    Win = true;
+                    EndGame = true;
+                }
+
+                lock (locker)
+                {
+                    Console.SetCursorPosition(0, 9 + counterPositionOnScreen);
+                    Console.Write("{0,4} {1}", LastMove, Thread.CurrentThread.Name);
+                }
+
+                counterPositionOnScreen++;
+                if (EndGame)
+                    break;
+              //  RandomPlayerHandler.Set();
+            }
+        }
+    }
+}
---
+++ HardWorkPlayerWithInheritanceThreads/HardWorkPlayerWithInheritance/RandomCheaterPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HardWorkPlayerWithInheritance
+{
+    class RandomCheaterPlayer:Players
+    {
+        public RandomCheaterPlayer()
+        {
+            Name = "RandomCheaterP";
+        }
+
+        public override void DoMove()
+        {
+            int counterPositionOnScreen = 0;
+
+            while (true)
+            {
+             //   RandomCheaterPlayerHandler.WaitOne();
+
+                int Rand = RandomNumber.Next(MinValue, MaxValue);
+
+                while (TestEnterdNumberInArray(Rand))
+                {
+                    Rand = RandomNumber.Next(MinValue, MaxValue);
+                }
+
+                AddEnterdNumberInArray(Rand);
+
+                if (Rand == RezultValue)
+                {
+                    Win = true;
+                    EndGame = true;
+                }
+
+                lock (locker)
+                {
+                    Console.SetCursorPosition(55, 9 + counterPositionOnScreen);
+                    Console.Write("{0,4} {1}", Rand, Thread.CurrentThread.Name);
+                }
+                counterPositionOnScreen++;
+                if (EndGame)
+                    break;
+                //  HardWorkingCheaterPlayerHandler.Set();
+            }
+        }
+
+    }
+}
